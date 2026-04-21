a7e486f4c7219c51d3b6508b8af224e0d2f4f777
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-04-21 21:48:30 +0200

    Add user input for location

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const cityEl = document.querySelector(".city")
-const longlatEl = document.querySelector(".longlat")
-const tempEl = document.querySelector(".temp")
+const cityEl = document.querySelector(".city");
+const longlatEl = document.querySelector(".longlat");
+const tempEl = document.querySelector(".temp");
 
+const form = document.querySelector(".form");
+const locationInput = document.querySelector("#location");
 
-async function getData() {
-  try {
-    const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Belgrade?unitGroup=metric&key=&contentType=json",
-    );
-    const data = await response.json();
-    console.log(data);
-    cityEl.textContent = data.address;
-    longlatEl.textContent = `Longitude: ${data.longitude} Latitude: ${data.latitude}`
-    tempEl.textContent = `${data.currentConditions.temp}°C`;
-  } catch (error) {
-    console.log(error);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = locationInput.value;
+
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=NHLJDJFWYWHFHCEK6ZYP53F9D&contentType=json`;
+  async function getData() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      cityEl.textContent = data.address;
+      longlatEl.textContent = `Longitude: ${data.longitude} Latitude: ${data.latitude}`;
+      tempEl.textContent = `${data.currentConditions.temp}°C`;
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
-
-getData();
+  getData();
+});
